f4742e258c85c4695c7cdcda08cf5368e9652a6a
thelostsoul5 <1095533751@qq.com>
2019-05-09 08:29:56 +0000

    update version

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,16 @@
 module.exports = {
   title: 'THE LOST SOUL',
   description: '饿，呆。',
-  dest: 'public'
+  dest: 'public',
+  head: [
+      ['link', { rel: 'icon', href: `/favicon.ico` }]
+  ],
+  // 主题的一些配置
+  themeConfig: {
+    nav: [
+        { text: 'HOME', link: '/', root: true }, // 指定它为博客根目录
+        { text: 'TAGS', link: '/tags/', tags: true }, // 指定它为标签目录
+        { text: 'BLOGS', link: '/_posts/' },
+    ]
+  }
 }
